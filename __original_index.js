797d2196459067c9a29509f564557d47336aae5b
FprumucenaBYUI <flaprumucena@byui.edu>
2022-03-30 00:42:25 -0600

    testing heroku

---
+++ __original_index.js
@@ -0,0 +1,72 @@
+
+const express = require('express')
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+require('dotenv').config()
+const port = 3000;
+
+
+// const vehicleRoute = require('./routes/__original__vehicleRoute');
+const vehicleRoute = require('./routes/vehicleRoute');
+const bodyParser = require('body-parser');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express')
+const router = express.Router();
+// const checkAuth = require('./middleware/auth');
+
+
+require('dotenv').config()
+const mongoose = require('mongoose');
+// const { postVehicleData, getVehicleData, getVehicleByID, deleteVehicle, updateVehicle, userSignup, userLogin } = require('./controllers/controllers');
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xsirj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
+
+mongoose.connection.on('error', err => {
+    console.log('Connection Failed');
+})
+
+mongoose.connection.on('connected', connected => {
+    console.log('Connection Successfully With database...')
+})
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+const options = {
+    definition: {
+        openapi: '3.0.0.',
+        info: {
+            title: 'Node js api project',
+            version: '1.0.0'
+        },
+        servers: [
+            {
+                
+                // url: 'http://localhost:3000/'
+                url: 'https://cse341-api-routes-team-03.herokuapp.com/'
+            }
+        ]
+    },
+    apis: ['./routes/*.js']
+}
+
+const swaggerSpec = swaggerJsdoc(options);
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
+router.use(vehicleRoute);
+// router.post('/userSignup', userSignup)
+// router.post('/userLogin', userLogin)
+
+app.use(router)
+
+app.use((req, res, next) => {
+    res.status(404).json({
+        error: 'Bad Request',
+    })
+})
+
+
+
+server.listen(process.env.PORT || port);
--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-
 const express = require('express')
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
-require('dotenv').config()
-const port = 3000;
+require('dotenv').config();
 
-
-// const vehicleRoute = require('./routes/__original__vehicleRoute');
-const vehicleRoute = require('./routes/vehicleRoute');
 const bodyParser = require('body-parser');
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express')
-const router = express.Router();
-// const checkAuth = require('./middleware/auth');
-
-
-require('dotenv').config()
+// const router = express.Router();
 const mongoose = require('mongoose');
-// const { postVehicleData, getVehicleData, getVehicleByID, deleteVehicle, updateVehicle, userSignup, userLogin } = require('./controllers/controllers');
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xsirj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
+const apiRoutes = require('./routes/vehicleRoute');
+const authRoute = require('./routes/auth');
+
+const PORT = process.env.PORT | 3000;
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.xsirj.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`)
 
 mongoose.connection.on('error', err => {
     console.log('Connection Failed');
@@ -38,28 +37,24 @@ const options = {
     definition: {
         openapi: '3.0.0.',
         info: {
-            title: 'Node js api project',
-            version: '1.0.0'
+            title: 'CS341 Rest API - Vehicle_Estimator_API',
+            version: '1.0.10'
         },
         servers: [
             {
-                
                 // url: 'http://localhost:3000/'
                 url: 'https://cse341-api-routes-team-03.herokuapp.com/'
             }
         ]
     },
-    apis: ['./routes/*.js']
+    apis: ['./index.js', './routes/*.js']
 }
 
 const swaggerSpec = swaggerJsdoc(options);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
-router.use(vehicleRoute);
-// router.post('/userSignup', userSignup)
-// router.post('/userLogin', userLogin)
-
-app.use(router)
+app.use(apiRoutes);
+app.use(authRoute);
 
 app.use((req, res, next) => {
     res.status(404).json({
@@ -68,5 +63,6 @@ app.use((req, res, next) => {
 })
 
 
-
-server.listen(process.env.PORT || port);
+server.listen(PORT, () => {
+    console.info(`Transport API running on Port ${PORT}`);
+})
