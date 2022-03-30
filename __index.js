752f24918cf346add690b2a76e36fc93d0fd6288
FprumucenaBYUI <flaprumucena@byui.edu>
2022-03-30 00:55:42 -0600

    testing heroku

--- __index.js
+++ __index.js
@@ -1,26 +1,26 @@
-
 const express = require('express')
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-require('dotenv').config()
-const vehicleRoutes = require('./routes/vehicleRoute');
-const authRoutes = require('./routes/auth');
+
+require('dotenv').config();
+
 const bodyParser = require('body-parser');
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express')
-const router = express.Router();
-
+// const router = express.Router();
+const mongoose = require('mongoose');
+const apiRoutes = require('./routes/vehicleRoute');
+const authRoute = require('./routes/auth');
 
 const PORT = process.env.PORT | 3000;
 const DB_NAME = process.env.DB_NAME;
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
-const SWAGGER_URL = PORT==3000?'http://localhost:3000/':'https://cse341-api-routes-team-03.herokuapp.com/';
 
-require('dotenv').config()
-const mongoose = require('mongoose');
-mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.xsirj.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`);
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.xsirj.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`)
+
 mongoose.connection.on('error', err => {
     console.log('Connection Failed');
 })
@@ -42,25 +42,26 @@ const options = {
         },
         servers: [
             {
-                url: SWAGGER_URL
+                // url: 'http://localhost:3000/'
+                url: 'https://cse341-api-routes-team-03.herokuapp.com/'
             }
         ]
     },
-    apis: ['./routes/*.js']
+    apis: ['./index.js', './routes/*.js']
 }
 
 const swaggerSpec = swaggerJsdoc(options);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
-router.use(vehicleRoutes);
-router.use(authRoutes);
-app.use(router)
+app.use(apiRoutes);
+app.use(authRoute);
 
 app.use((req, res, next) => {
     res.status(404).json({
         error: 'Bad Request',
     })
 })
+
 
 server.listen(PORT, () => {
     console.info(`Transport API running on Port ${PORT}`);
